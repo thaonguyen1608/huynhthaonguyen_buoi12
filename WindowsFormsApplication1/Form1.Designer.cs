36ae3f5cc83651c33af46ba4085febbe7499a570
Huynh Thao Nguyen <Nuyenml0984@gmail.com>
2023-12-06 14:26:22 +0700

    new file

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.lb2 = new System.Windows.Forms.Label();
+            this.txtMaSV = new System.Windows.Forms.TextBox();
+            this.txtTenSV = new System.Windows.Forms.TextBox();
+            this.dgvDSSV = new System.Windows.Forms.DataGridView();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(38, 32);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(66, 13);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "ma sinh vien";
+            // 
+            // lb2
+            // 
+            this.lb2.AutoSize = true;
+            this.lb2.Location = new System.Drawing.Point(38, 62);
+            this.lb2.Name = "lb2";
+            this.lb2.Size = new System.Drawing.Size(67, 13);
+            this.lb2.TabIndex = 1;
+            this.lb2.Text = "ten sinh vien";
+            // 
+            // txtMaSV
+            // 
+            this.txtMaSV.Location = new System.Drawing.Point(136, 29);
+            this.txtMaSV.Name = "txtMaSV";
+            this.txtMaSV.Size = new System.Drawing.Size(112, 20);
+            this.txtMaSV.TabIndex = 4;
+            // 
+            // txtTenSV
+            // 
+            this.txtTenSV.Location = new System.Drawing.Point(136, 59);
+            this.txtTenSV.Name = "txtTenSV";
+            this.txtTenSV.Size = new System.Drawing.Size(112, 20);
+            this.txtTenSV.TabIndex = 5;
+            // 
+            // dgvDSSV
+            // 
+            this.dgvDSSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDSSV.Location = new System.Drawing.Point(22, 99);
+            this.dgvDSSV.Name = "dgvDSSV";
+            this.dgvDSSV.Size = new System.Drawing.Size(406, 150);
+            this.dgvDSSV.TabIndex = 6;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(15, 284);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 23);
+            this.btnThem.TabIndex = 7;
+            this.btnThem.Text = "Them";
+            this.btnThem.UseVisualStyleBackColor = true;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(117, 284);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 23);
+            this.btnXoa.TabIndex = 8;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            // 
+            // btnSua
+            // 
+            this.btnSua.Location = new System.Drawing.Point(235, 284);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 23);
+            this.btnSua.TabIndex = 9;
+            this.btnSua.Text = "Sua";
+            this.btnSua.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(341, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Tim kiem";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 340);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.dgvDSSV);
+            this.Controls.Add(this.txtTenSV);
+            this.Controls.Add(this.txtMaSV);
+            this.Controls.Add(this.lb2);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDSSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.Label lb2;
+        private System.Windows.Forms.TextBox txtMaSV;
+        private System.Windows.Forms.TextBox txtTenSV;
+        private System.Windows.Forms.DataGridView dgvDSSV;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Bai1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter da_sinhvien;
+        DataSet ds_sinhvien;
+        DataColumn[] key = new DataColumn[1];
+        SqlCommand cmd = new SqlCommand();
+        string strsql;
+        public Form1()
+        {
+            InitializeComponent();
+            conn = new SqlConnection("Data Source=A209PC03;Initial Catalog=Sinhvien;Integrated Security=True");
+            string strSelect = "select * from SinhVien";
+            da_sinhvien = new SqlDataAdapter(strSelect, conn);
+            ds_sinhvien = new DataSet();
+            da_sinhvien.Fill(ds_sinhvien, "SinhVien");
+            key[0] = ds_sinhvien.Tables["SinhVien"].Columns[0];
+            ds_sinhvien.Tables["SinhVien"].PrimaryKey = key;
+        }
+        void Databingding(DataTable pDT)
+        {
+            txtMaSV.DataBindings.Clear();
+            txtTenSV.DataBindings.Clear();
+
+            txtMaSV.DataBindings.Add("Text", pDT, "MaSinhVien");
+            txtTenSV.DataBindings.Add("Text", pDT, "TenSinhVien");
+        }
+        void loadGrid()
+        {
+            dgvDSSV.DataSource = ds_sinhvien.Tables[0];
+            Databingding(ds_sinhvien.Tables[0]);
+        }
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            DataRow newrow = ds_sinhvien.Tables[0].NewRow();
+            newrow["MaSinhvien"] = txtMaSV.Text;
+            newrow["TenSinhvien"] = txtTenSV.Text;
+            ds_sinhvien.Tables[0].Rows.Add(newrow);
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_sinhvien);
+            da_sinhvien.Update(ds_sinhvien, "SinhVien");
+            txtMaSV.Clear();
+            txtTenSV.Clear();
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds_sinhvien.Tables[0].Rows.Find(lb1.Text);
+            if (dr != null)
+            {
+                dr.Delete();
+            }
+            //cap nhap trong database
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_sinhvien);
+            //cap nhat dataset
+            da_sinhvien.Update(ds_sinhvien, "Sinhvien");
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds_sinhvien.Tables[0].Rows.Find(lb1.Text);
+            if (dr != null)
+            {
+                dr["TenSinhvien"] = lb2.Text;
+            }
+            SqlCommandBuilder cB = new SqlCommandBuilder(da_sinhvien);
+            da_sinhvien.Update(ds_sinhvien, "Sinhvien");
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadGrid();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            loadGrid();
+        }
+    }
+}
